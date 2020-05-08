9b8bd52f2205b9fc723b9340702ac14342cd2c50
radrigo <cherkashin.da@gazprom-neft.ru>
2020-05-08 15:16:17 +0300

    create component question

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 import './Quiz.css';
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 class Quizz extends Component {
     constructor(props) {
         super(props);
-        this.state = [];
+        //this.state = [];
       }
 
     render() {
         return (
             <div className="Quiz">
-
+                <div className="QuizWrapper">
+                    <h1> Quiz</h1>
+                    <ActiveQuiz />
+                </div>
             </div>
         );
     };
