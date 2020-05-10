e9ac35d9c0b8105766a71304b54716f9b5a62836
radrigo <cherkashin.da@gazprom-neft.ru>
2020-05-10 20:30:28 +0300

    create menu toggle component

---
+++ src/components/Navigation/MenuToggle/MenuToggle.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './MenuToggle.css';
+
+const MenuToggle = props => {
+    const classes = [
+        'MenuToggle',
+        'fa'
+    ];
+
+    if(props.isOpen) {
+        classes.push('fa-times');
+        classes.push('open');
+    } else {
+        classes.push('fa-bars');
+    }
+
+    return (
+        <i
+            className={classes.join(' ')}
+            onClick={props.onToggle}
+        >
+
+        </i>
+    );
+}
+
+export default MenuToggle;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 import './Layout.css';
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
 
 class Layout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menu: false
+        }
+
+        this.toggleMenuHandler = this.toggleMenuHandler.bind(this);
+    }
+
+    toggleMenuHandler() {
+        this.setState({
+            menu: !this.state.menu
+        });
+    }
+
     render() {
         return (
             <div className="Layout">
+                <MenuToggle 
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
+
                 <main>
                     { this.props.children }
                 </main>
