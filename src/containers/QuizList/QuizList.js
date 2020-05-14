aa1623c6fbed1196d467509bf6c94f28726245e7
radrigo <cherkashin.da@gazprom-neft.ru>
2020-05-14 18:14:10 +0300

    implement quiz list component

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 import './QuizList.css';
+import {NavLink} from 'react-router-dom';
 
 class QuizList extends Component {
+    renderQuizes() {
+        return[1, 2, 3].map((quiz, index) => {
+            return (
+                <li key={index}>
+                <NavLink 
+                    to={'/quiz/' + quiz}
+                >
+                    Тест {quiz}
+                </NavLink>
+            </li>
+            );
+        });
+    }
+
     render() {
         return (
-            <div>List</div>
+            <div className="QuizList">
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
