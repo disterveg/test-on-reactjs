76ffc1c6cecf6909d9c27bbfd3d4ceef10e37ef6
radrigo <cherkashin.da@gazprom-neft.ru>
2020-05-14 17:30:07 +0300

    install routing create components

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './QuizCreator.css';
+
+class QuizCreator extends Component {
+    render() {
+        return (
+            <div>Creator</div>
+        );
+    }
+}
+
+export default QuizCreator;
---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './QuizList.css';
+
+class QuizList extends Component {
+    render() {
+        return (
+            <div>List</div>
+        );
+    }
+}
+
+export default QuizList;
