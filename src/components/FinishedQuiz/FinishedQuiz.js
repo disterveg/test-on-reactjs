75ff6b493b93fa56bc3975fa31253f377fbb30f1
radrigo <cherkashin.da@gazprom-neft.ru>
2020-05-15 10:30:27 +0300

    add nav on button

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './FinishedQuiz.css';
 import Button from '../UI/Button/Button';
+import {withRouter} from 'react-router-dom';
 
 const FinishedQuiz = props => {
     const countSuccess = Object.keys(props.results).reduce((total, key) => {
@@ -34,10 +35,11 @@ const FinishedQuiz = props => {
         <p>Правильно {countSuccess} из {props.quiz.length}</p>
 
         <div>
-            <Button onClick={props.onRetry} type="success">Повторить</Button>
+            <Button onClick={props.onRetry} type="primary">Повторить</Button>
+            <Button onClick={() => props.history.push('/')} type="success">Перейти в список тестов</Button>
         </div>
     </div>
 );
 }
 
-export default FinishedQuiz;
+export default withRouter(FinishedQuiz);
