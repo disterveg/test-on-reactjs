0b7c88946023b13da29e2eee2abaa4422ccde49b
radrigo <cherkashin.da@gazprom-neft.ru>
2020-05-16 16:10:24 +0300

    create auth component and input

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Input.css';
+
+function isInvalid({valid, touched, shouldValidate}) {
+    return !valid && shouldValidate && touched;
+};
+
+const Input = props => {
+   const inputType = props.type || 'text';
+   const classses = [
+       'Input'
+   ];
+   const htmlFor = `${inputType}-${Math.random()}`;
+
+   if(isInvalid(props)) {
+       classses.push('invalid');
+   }
+
+    return (
+        <div className={classses.join(' ')}>
+            <label htmlFor={htmlFor}>{props.label}</label>
+            <input 
+                type={inputType}
+                id={htmlFor}
+                value={props.value}
+                onChange={props.onChange}
+            />
+
+            {
+                isInvalid(props) 
+                ? <span>{props.errorMessage || 'Неккоректное значение'}</span>
+                : null
+            }
+        </div>
+    );
+}
+
+export default Input;
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,56 @@
 import React, {Component} from 'react';
 import './Auth.css';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 class Auth extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        this.SubmitHandler = this.SubmitHandler.bind(this);
+        this.loginHandler = this.loginHandler.bind(this);
+        this.registerHandler = this.registerHandler.bind(this);
+    }
+
+    SubmitHandler() {
+
+    }
+
+    loginHandler() {
+
+    }
+
+    registerHandler() {
+
+    }
+
     render() {
         return (
-            <div>Auth</div>
+            <div className="Auth">
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.SubmitHandler} className="AuthForm">
+                        <Input label="Email"/>
+                        <Input label="Пароль"/>
+
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                        >
+                            Войти
+                        </Button>
+
+                        <Button
+                            type="primary"
+                            onClick={this.registerHandler}
+                        >
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
